3f5af70b78e3bce377c516a0412784251a98bba5
mleukam <mleukam@gmail.com>
2019-06-13 13:58:00 -0500

    gsva R scripts

---
+++ code/gsva1.r
@@ -0,0 +1,43 @@
+## -----------------------------------------------------
+
+## R script to get GSVA scores on all TCGA RNAseq reads from solid tumors
+
+## Expects cleaned, normalized, log-transformed, expression matrix ready for GSVA analysis
+##    Originally defined using HTseq reads from GDC
+##    Expression input to GSVA may be expressionset object or matrix
+## Expects gene lists as a list of character vectors for each gene set
+##    Gene list input must be a list
+## Outputs matrix of GSVA scores for each sample (columns) and gene set (rows)
+
+## M. Leukam
+## 6/12/2019
+
+## -----------------------------------------------------
+
+# clear workspace
+rm(list = ls())
+
+# load packages
+library("tidyverse")
+library("GSVA")
+
+# read in data
+total_reads <- read_rds("~/gpfs/data/kline-lab/tcga_macs/data/selected.total.counts.rds")
+gset <- read_rds("~/gpfs/data/kline-lab/tcga_macs/data/gset_ids_complete.rds")
+
+# set up expression matrix
+rownames <- total_reads %>% pull(gene)
+expr_matrix <- total_reads %>%
+  dplyr::select(-gene) %>%
+  as.matrix()
+rownames(expr_matrix) <- rownames
+
+# get gvsa scores
+tcga_es <- gsva(expr_matrix, gset,
+                annotation = NULL,
+                method = "gsva",
+                mx.diff = TRUE,
+                verbose = TRUE)
+
+# write out results
+saveRDS(tcga_es, "/gpfs/data/kline-lab/tcga_macs/output/tcga_gsva_17geneset_results.rds")
---
+++ code/gsva1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash/
+
+###############################
+## Shell script for tximport ##
+###############################
+
+## Set script to fail if any command, variable, or output fails
+set -euo pipefail
+
+## Set IFS to split only on newline and tab
+IFS=$'\n\t' 
+
+## Load compiler
+module load gcc/6.2.0
+
+## Load module
+module load R/3.4.1
+
+## Navigate to directory containing R script
+cd /home/mleukam/R/Rscripts/
+
+## Call R script
+Rscript gsva1.r && exit 0
+
+## Exit if error
+exit 1
